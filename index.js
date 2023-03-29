03e64ba8d15fbd15e6e2122622babfb4fa6fda67
Laiamdelprado <lmarcodelprado@gmail.com>
2023-03-29 09:35:44 +0200

    feat(): complete contact-form for technical test

---
+++ index.js
@@ -0,0 +1,15 @@
+function onConfirm() {
+    Swal.fire({
+        title: 'Gracias!',
+        text: 'Tu formulario ha sido enviado',
+        imageUrl: 'https://cdn-icons-png.flaticon.com/512/845/845646.png?w=740&t=st=1680072838~exp=1680073438~hmac=8aaaa710675b8839f6e977feaca256b344fd23da19ff0ff8496f24b8904054c1',
+        imageWidth: 100,
+        imageHeight: 100,
+        imageAlt: 'Custom image',
+        showConfirmButton: true,
+        confirmButtonColor: "#fb716c",
+    })
+
+
+}
+
